91dcfacb5c0e6a1890339bbc5409616feb0dba8d
jakob <jakob@dashsoft.dk>
2016-11-14 08:27:17 +0100

    Initial commit. https://dashsoft.dk/en/blog/HTTP-protocol-using-NETC/

---
+++ StreamingHttpClient/StreamingHttpClient.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StreamingHttpClient
+{
+    class StreamingHttpClient
+    {
+
+        // Prior to 2014 RFC2616 described HTTP/1.1.
+        // Post 2014 HTTP1/1 was described by multiple RFCs (7230-7237)
+
+
+        public struct Response
+        {
+            public string body;
+            public bool success;
+            public double elapsedMilliseconds;
+        }
+
+
+        private readonly string _userAgent;
+        private readonly int _readTimeout;
+        private Uri _host;
+        private NetworkStream networkStream;
+        private TcpClient tcpClient;
+
+        public StreamingHttpClient(string userAgent = "StreamingHttpClient/0.0.1",
+                                   int readTimeout = 1000)
+        {
+            this._userAgent = userAgent;
+            this._readTimeout = readTimeout;
+        }
+
+
+        public bool Connected => tcpClient?.Connected ?? false;
+
+        public bool Disconnect()
+        {
+            networkStream?.Close();
+            tcpClient?.Close();
+
+            return !tcpClient?.Connected ?? false;
+        }
+
+        public async Task<bool> Connect(Uri server)
+        {
+            this._host = server;
+
+            networkStream?.Close();
+            tcpClient?.Close();
+
+            tcpClient = new TcpClient { NoDelay = true };
+            try
+            {
+                await tcpClient.ConnectAsync(_host.Host, _host.Port);
+            }
+            catch (SocketException socketException)
+            {
+                return false;
+            }
+            networkStream = tcpClient.GetStream();
+            networkStream.ReadTimeout = _readTimeout;
+
+            return tcpClient.Connected;
+        }
+
+        public async Task<bool> SendRequest(string request, string path = "/", string contentType = "text/xml")
+        {
+            return await SendRequest(request, (new CancellationTokenSource()).Token, path, contentType);
+        }
+
+        public async Task<bool> SendRequest(string request, CancellationToken cancellationToken, string path = "/", string contentType = "text/xml")
+        {
+            Console.WriteLine("\n\nPosting: \n" + request + "\n\n");
+            var headers = "POST " + path + " HTTP/1.1\r\n" +
+                          "Connection: keep-alive\r\n" +
+                          "Content-Type: " + contentType + "\r\n" +
+                          "Content-Length: " + Encoding.UTF8.GetByteCount(request) + "\r\n" +
+                          "Host: " + _host.Host + "\r\n" +
+                          "Accept: */*\r\n" +
+                          "Accept-Encoding: identity\r\n" +
+                          "User-Agent: " + _userAgent + "\r\n\r\n" +
+                          request;
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(headers);
+
+            try
+            {
+                await networkStream.WriteAsync(byteArray, 0, byteArray.Length, cancellationToken);
+            }
+            catch (Exception e)
+            {
+                //todo: Better error handling
+                return false;
+            }
+            return true;
+        }
+
+        public async Task<Response> GetResponse(int timeout = 1000)
+        {
+            return await GetResponse(timeout, (new CancellationTokenSource()).Token);
+        }
+
+        public async Task<Response> GetResponse(int timeout, CancellationToken cancellationToken)
+        {
+            Console.WriteLine("\n\nWaiting for Response ...\n");
+            bool success = false;                               // Response HTTP code 2xx ?
+            bool readingBody = false;                           // StreamReader reached body yet?
+            int bytesToRead = 0;                                // Content-Length value
+            DateTime beginRead = DateTime.Now;                  // Used for timeout purposes
+            //todo: Find better structure to save body in
+            StringBuilder stringBuilder = new StringBuilder();  // Used for storing body of response
+
+            networkStream.ReadTimeout = timeout;
+
+            while ((DateTime.Now - beginRead).TotalMilliseconds < timeout && !cancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    using (StreamReader streamReader = new StreamReader(networkStream, Encoding.UTF8, true, 1024, true))
+                    {
+                        while (!streamReader.EndOfStream && !cancellationToken.IsCancellationRequested)
+                        {
+                            var line = await streamReader.ReadLineAsync();
+
+                            Console.WriteLine(line);
+
+                            // Does our head tell us everything is OK?
+                            if (line.ToLower().Contains("http/") && Regex.IsMatch(line, "(2\\d\\d)"))
+                                success = true;
+
+                            // How big is our body?
+                            if (line.ToLower().Contains("content-length"))
+                            {
+                                try
+                                {
+                                    bytesToRead = Int32.Parse(line.Split(':')[1]);
+                                }
+                                catch (Exception e)
+                                {
+                                    //todo: Better error reporting
+                                    break;
+                                }
+                            }
+
+                            // Gather up body response
+                            if (readingBody) stringBuilder.AppendLine(line);
+
+                            // Detect neck
+                            if (line.Equals("")) readingBody = true;
+
+                            // Detect end of body
+                            if (readingBody && Encoding.UTF8.GetBytes(stringBuilder.ToString()).Length >= bytesToRead)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    return new Response()
+                    {
+                        body = stringBuilder.ToString(),
+                        success = success,
+                        elapsedMilliseconds = (DateTime.Now - beginRead).TotalMilliseconds
+                    };
+                }
+                catch (Exception e)
+                {
+                    // Calling EndOfStream on empty StreamReader => Exception!
+                }
+            }
+
+            return new Response()
+            {
+                body = stringBuilder.ToString(),
+                success = success,
+                elapsedMilliseconds = (DateTime.Now - beginRead).TotalMilliseconds
+            };
+        }
+    }
+}
---
+++ StreamingHttpClient/StreamingHttpClientTest.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace StreamingHttpClient
+{
+
+    class StreamingHttpClientTest
+    {
+        private static readonly Uri ApiUri      = new Uri("http://localhost:8080/");
+        private const string LoginMethod        = "loginSubscriber";
+        private const string SubscribeMethod    = "subscribeTeesheetUpdate";
+        private const string Username           = "username";
+        private const string Password           = "P4s5W0rd!";
+        private bool connect = true;
+        public static void Main(string[] args)
+        {
+            StreamingHttpClientTest streamingHttpClientTest = new StreamingHttpClientTest();
+
+            streamingHttpClientTest.StartListener();
+            streamingHttpClientTest.StartClient();
+
+            Console.Read();
+        }
+
+        //=========================================================================================================
+        // Client test below
+        //=========================================================================================================
+
+        private async void StartClient()
+        {
+            Console.WriteLine("\n\n" +
+                              "\t\t=========================================\n" +
+                              "\t\t*** Starting StreamingHttpClient test ***\n" +
+                              "\t\t=========================================\n" +
+                              "\n\n");
+
+            // Setup Streaming HTTP Client with timeout of 15 minutes
+            var streamingHttpClient = new StreamingHttpClient(readTimeout: 15*60*1000);
+            bool status = await streamingHttpClient.Connect(ApiUri);
+
+            if (!status)
+            {
+                Console.WriteLine("[Client] Connecting to " + ApiUri + " failed!");
+                return;
+            }
+
+            // Login
+            var loginRequest = Encoding.UTF8.GetString(CreatePayload(LoginMethod, new[] {Username, Password}));
+
+            Console.WriteLine("[Client] **\tLogging in ... ");
+            await streamingHttpClient.SendRequest(loginRequest);
+            var loginResponse = await streamingHttpClient.GetResponse();
+            if (!loginResponse.success)
+            {
+                Console.WriteLine("Login FAILED!");
+                throw new Exception("Login failed!");
+            }
+            var _token = ParseLogin(loginResponse.body);
+
+            
+            // Subscribe
+            var subscribeRequest = Encoding.UTF8.GetString(CreatePayload(SubscribeMethod, new[] {_token}));
+            
+            Console.WriteLine("[Client] **\tSubscribing ... ");
+            await streamingHttpClient.SendRequest(subscribeRequest);
+            var subscribeResponse = await streamingHttpClient.GetResponse();
+            if (!subscribeResponse.success)
+            {
+                Console.WriteLine("Subscribe FAILED!");
+                throw new Exception("Subscribe failed!");
+            }
+
+
+            // Wait for updates for our tee-times
+            while (connect)
+            {
+                var updateResponse = await streamingHttpClient.GetResponse(1800000);
+                if (updateResponse.success)
+                {
+                    var updateCount = ParseUpdates(updateResponse.body);
+                    Console.WriteLine("[Client] Received " + updateCount + " updates in " + updateResponse.elapsedMilliseconds +
+                                      "ms.");
+                }
+                else
+                {
+                    Console.WriteLine("[Client] Failed getting updates: " + updateResponse.body);
+                }
+                Thread.Sleep(250);
+            }
+        }
+
+        private int ParseUpdates(string body)
+        {
+            try
+            {
+                XDocument xDocument = XDocument.Parse(body);
+                return xDocument.XPathSelectElements("/pushResponse/data/item").Count();
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        private string ParseLogin(string body)
+        {
+            #region examples
+
+            /* Example GOOD response:
+            <?xml version="1.0" encoding="UTF-8"?>
+
+            <methodResponse>
+              <params>
+                <param>
+                  <value>
+                    <string>dmlzaXRkdWJhaSwydDE2LTA4LTizIDAxOjEzOjUxLjCzM3wweDAweDAxMTzD</string>
+                  </value>
+                </param>
+              </params>
+            </methodResponse>
+
+            <!-- XPATH expression: /methodResponse/params/param/value/string -->
+            */
+
+            /* Example BAD response:
+            <?xml version="1.0"?>
+
+            <methodResponse>
+              <fault>
+                <value>
+                  <struct>
+                    <member>
+                      <name>faultCode</name>
+                      <value><int>999</int></value>
+                    </member>
+                    <member>
+                      <name>faultString</name>
+                      <value><string>Item is not a string type</string></value>
+                    </member>
+                  </struct>
+                </value>
+              </fault>
+            </methodResponse>
+            
+            <!-- XPATH expressions:
+                /methodResponse/fault/value/struct/member/value/int
+                /methodResponse/fault/value/struct/member/value/string
+                -->
+            */
+
+            #endregion
+
+            try
+            {
+                XDocument xDocument = XDocument.Parse(body);
+                return xDocument.XPathSelectElement("/methodResponse/params/param/value/string").Value;
+            }
+            //todo: Better error reporting
+            catch (Exception e)
+            {
+                return String.Empty;
+            }
+        }
+
+        public byte[] CreatePayload(String method, IEnumerable<string> methodParameters)
+        {
+            XElement paramsXElement = new XElement("params");
+            foreach (var methodParameter in methodParameters)
+            {
+                paramsXElement.Add(new XElement("param",
+                                        new XElement("value",
+                                            new XElement("string", methodParameter))));
+            }
+
+            return Encoding.UTF8.GetBytes(
+                new XElement("methodCall",
+                    new XElement("methodName", method),
+                        paramsXElement).ToString()
+                );
+        }
+
+        //=========================================================================================================
+        //  Server mock below
+        //=========================================================================================================
+
+        public bool Subscribed { get; set; }
+        public NetworkStream ServerNetworkStream { get; set; }
+
+        private async void StartListener()
+        {
+            Subscribed = false;
+            //NB: This listener can only handle one connection at a time.
+            TcpListener tcpListener = TcpListener.Create(8080);
+            tcpListener.Start();
+            
+            var tcpClient = await tcpListener.AcceptTcpClientAsync();
+
+            ServerNetworkStream = tcpClient.GetStream();
+            using (StreamReader streamReader = new StreamReader(ServerNetworkStream))
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                bool bodyReached = false;
+                int bytesToRead = 0;
+                while (!streamReader.EndOfStream)
+                {
+                    var line = await streamReader.ReadLineAsync();
+                    
+                    // New request received
+                    if (line.ToLower().Contains("post") || line.ToLower().Contains("get"))
+                    {
+                        // NB: Resetting here as well, in case previous request didn't complete
+                        bodyReached = false;
+                        bytesToRead = 0;
+                        stringBuilder = new StringBuilder();
+                    }
+
+                    // How much of the body should be read before we can expect new request
+                    if (line.ToLower().Contains("content-length"))
+                    {
+                        if (!Int32.TryParse(line.Split(':')[1], out bytesToRead)) bytesToRead = 0;
+                        
+                    }
+
+                    // Reading body? Collect body request
+                    if (bodyReached) stringBuilder.AppendLine(line);
+                    // Found neck! Reached body.
+                    if (line.Equals("")) bodyReached = true;
+                    // If body isn't reached, wait for body. If body.bytes.length < bytesToRead -> keep collecting body
+                    if (!bodyReached ||
+                        Encoding.UTF8.GetByteCount(stringBuilder.ToString()) < bytesToRead) continue;
+                    
+                    // Respond to the request
+                    byte[] buffer = ApplyHttpHeaders(ServerParseRequest(stringBuilder.ToString()));
+                    await ServerNetworkStream.WriteAsync(buffer, 0, buffer.Length);
+
+                    // This request is complete, reset for new request.
+                    bodyReached = false;
+                    bytesToRead = 0;
+                    stringBuilder = new StringBuilder();
+                }
+            }
+
+        }
+
+        private string ServerParseRequest(string requestPayload)
+        {
+            try
+            {
+                XDocument xDocument = XDocument.Parse(requestPayload);
+                string methodCall = xDocument.XPathSelectElement("/methodCall/methodName").Value;
+                Console.WriteLine("[Server] Received " + methodCall);
+                switch (methodCall)
+                {
+                    case "loginSubscriber":
+                        return CreateResponse(Encoding.UTF8.GetBytes("R29sZk5vdywweDAwMDAwRjVD"));
+                    case "subscribeTeesheetUpdate":
+                        this.Subscribed = true;
+                        Task.Factory.StartNew(RandomSendUpdate);
+
+                        return CreateResponse(Encoding.UTF8.GetBytes("Subscription received for " + Username));
+                    default:
+                        return CreateResponse(Encoding.UTF8.GetBytes("Something went wrong.."));
+                }
+            }
+            catch (Exception e)
+            {
+                return CreateResponse(Encoding.UTF8.GetBytes(e.Message));
+            }
+        }
+
+        private async void RandomSendUpdate()
+        {
+            while (!Subscribed) Thread.Sleep(100);
+
+            while (Subscribed && connect)
+            {
+                int randomSleep = (new Random()).Next(10000);
+                Console.WriteLine("[Server] Sending Updates in " + randomSleep + "ms");
+                Thread.Sleep(randomSleep);
+
+                byte[] buffer = ApplyHttpHeaders(CreateUpdateResponse());
+                Console.WriteLine("[Server] Sending Updates");
+                await ServerNetworkStream.WriteAsync(buffer, 0, buffer.Length);
+            }
+        }
+
+        private byte[] ApplyHttpHeaders(string payload)
+        {
+            return Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n" +
+                                          "Connection: keep-alive\r\n" +
+                                          "Content-Type: text/xml\r\n" +
+                                          "Content-Length: " + Encoding.UTF8.GetByteCount(payload) + "\r\n" +
+                                          "Server: StreamingHttpServer\r\n" +
+                                          "\r\n" +
+                                          payload +
+                                          "\r\n");
+        }
+
+        private string CreateUpdateResponse()
+        {
+            int randomItemCount = (new Random()).Next(1000);
+
+            var data = new XElement("data");
+            for (int i = 0; i < randomItemCount; i++)
+            {
+                data.Add(new XElement("item", i));
+            }
+
+            return (new XElement("pushResponse", data)).ToString();
+        }
+
+        private string CreateResponse(byte[] input)
+        {
+            return new XElement("methodResponse",
+                    new XElement("params",
+                        new XElement("param",
+                            new XElement("value",
+                                new XElement("string", Encoding.UTF8.GetString(input)))))).ToString();
+        }
+    }
+}
